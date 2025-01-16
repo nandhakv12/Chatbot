00f4c39eed48d3f238fafe06411c4a8ce158f5b5
nandhakv12 <91311782+nandhakv12@users.noreply.github.com>
2025-01-16 00:17:07 -0500

    folder added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Chatbot Project',
+    version= '1.0.0',
+    author= 'Nandhakumar',
+    author_email= 'nandhakv12@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
